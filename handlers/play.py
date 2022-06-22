dfffec94544694296efbc963d1d77866970eee16
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2022-06-22 12:01:01 +0400

    update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -306,7 +306,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="final.png",
         reply_markup=keyboard,
-        caption="**📡 Yayım başladı 💡\n\n💡 Başlıq:** {}\n**🕒 Müddət:** {} min\n**👤 Tələb:** {}\n\n**\n\n**✅İndi hal-hazırda `{}`...**".format(
+        caption="**📡 Yayım başladı 💡\n\n💡 Başlıq:** {}\n**🕒 Müddət:** {} min\n**👤 Tələb:** {}**\n\n**✅İndi hal-hazırda `{}`...**".format(
         title, duration, message.from_user.mention(), message.chat.title
         ), )
         os.remove("final.png")
