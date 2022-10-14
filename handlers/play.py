42b40183e9178137bacf1b92a52282885d149547
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-10-15 01:18:54 +0400

    update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -286,7 +286,7 @@ async def play(_, message: Message):
         position = await queues.put(message.chat.id, file=file_path)
         await message.reply_photo(
         photo="final.png",
-        caption="**▶️ Səsdə oxunur...\n\n🎵 Başlıq:** {}\n**🕒 Müddət:** {}\n**👤 İstəyən:** {}\n\n**⏭️ Növbə:** {}".format(
+        caption="**🎵 Başlıq:** {}\n**🕒 Müddət:** {}\n**👤 İstəyən:** {}\n\n**⏭️ Növbə:** {}".format(
         title, duration, message.from_user.mention(), position
         ),
         reply_markup=keyboard)
@@ -306,7 +306,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="final.png",
         reply_markup=keyboard,
-        caption="**🔂 Növbədə oxunacaq...\n\n🎵 Başlıq:** {}\n**🕒 Müddət:** {}\n**👤 İstəyən:** {}".format(
+        caption="**🎵 Başlıq:** {}\n**🕒 Müddət:** {}\n**👤 İstəyən:** {}".format(
         title, duration, message.from_user.mention(), message.chat.title
         ), )
         os.remove("final.png")
