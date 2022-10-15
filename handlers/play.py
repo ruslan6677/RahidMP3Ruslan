c77760fd7b3f29aab0a29026f7db41c44da9958b
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-10-15 19:27:18 +0400

    update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -198,10 +198,10 @@ async def play(_, message: Message):
             keyboard = InlineKeyboardMarkup(
                       [
             [
-                InlineKeyboardButton("🤖 Digər Botlar", url=f"https://t.me/Rahid_44"),
-                InlineKeyboardButton("🆘 Support", url=f"https://t.me/Rahid_Support"),
+                InlineKeyboardButton("✅ Qrupa Əlavə Et", url=f"https://t.me/Rahid_Music_Bot?startgroup=a"),
+                InlineKeyboardButton("🌐 Digər Botlar", url=f"https://t.me/Rahid_44"),
             ],[
-                InlineKeyboardButton("🗑️ Menyunu Bağla", callback_data="cls"),
+                InlineKeyboardButton("🗑️ Bağla", callback_data="cls"),
             ],
         ]
     )
@@ -263,10 +263,10 @@ async def play(_, message: Message):
         keyboard = InlineKeyboardMarkup(
                       [
             [
-                InlineKeyboardButton("🤖 Digər Botlar", url=f"https://t.me/Rahid_44"),
-                InlineKeyboardButton("🆘 Support", url=f"https://t.me/Rahid_Support"),
+                InlineKeyboardButton("✅ Qrupa Əlavə Et", url=f"https://t.me/Rahid_Music_Bot?startgroup=a"),
+                InlineKeyboardButton("🌐 Digər Botlar", url=f"https://t.me/Rahid_44"),
             ],[
-                InlineKeyboardButton("🗑️ Menyunu Bağla", callback_data="cls"),
+                InlineKeyboardButton("🗑️ Bağla", callback_data="cls"),
             ],
         ]
     )
